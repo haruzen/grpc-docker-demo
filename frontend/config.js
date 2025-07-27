0196b2b4868c0253b31876d4096f69fdb7a5688f
Toan Xuan Bui <toan.bui@hitachids.com>
2025-07-28 01:37:26 +0700

    add config.js environment variables for frontend

--- frontend/config.js
+++ frontend/config.js
@@ -12,6 +12,6 @@ if (hostname === "localhost") {
   window._env_ = {
     COGNITO_DOMAIN: "https://ap-southeast-2v3udzxxtw.auth.ap-southeast-2.amazoncognito.com",
     CLIENT_ID: "7jjhsd6m1q3um03106k9l3b15k",
-    REDIRECT_URI: "http://3.106.127.61/frontend/index.html"
+    REDIRECT_URI: "https://3.106.127.61/frontend/index.html"
   };
 }
